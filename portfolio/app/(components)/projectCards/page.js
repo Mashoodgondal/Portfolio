53b34484a44a1669bcf5240ac78a3c3eb6f34c07
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-02-08 00:42:11 +0500

    animate project cards

--- portfolio/app/(components)/projectCards/page.js
+++ portfolio/app/(components)/projectCards/page.js
@@ -1,7 +1,7 @@
 
 export default function ProjectsCard() {
     return (
-        <div className=" shadow-md hover:shadow-purple-900 hover:shadow-2xl transition-shadow duration-500 group">
+        <div className="shadow-md hover:shadow-purple-900 hover:shadow-2xl transition-shadow duration-500 group transform hover:rotate-x-6 hover:rotate-y-6 hover:duration-300">
             <div className="relative group rounded-xl overflow-hidden shadow-lg">
 
                 <img
@@ -9,7 +9,6 @@ export default function ProjectsCard() {
                     alt="Sample Image"
                     className="w-full h-64 object-cover transition-opacity duration-300 group-hover:opacity-50"
                 />
-
 
                 <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                     <div className="flex space-x-4">
@@ -23,15 +22,10 @@ export default function ProjectsCard() {
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7"></path>
                             </svg>
                         </button>
-                        {/* <button className="bg-white p-3 rounded-full shadow-lg hover:bg-green-500 hover:text-white transition-colors duration-300">
-                            <svg className="w-6 h-6" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M3 10h11M9 21V3"></path>
-                            </svg>
-                        </button> */}
                     </div>
                 </div>
 
-                <div className="bg-white py-4 text-center">
+                <div className="bg-indigo-400 py-4 text-center">
                     <h2 className="text-lg font-bold text-gray-700">Beautiful Scenery</h2>
                 </div>
             </div>
