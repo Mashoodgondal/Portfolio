2b363cd1c65b1e6250f63524c4196db1410873c2
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-05-05 23:25:46 +0500

    pass names

--- portfolio/app/projects/page.js
+++ portfolio/app/projects/page.js
@@ -5,10 +5,10 @@ import ProjectsCard from '../(components)/projectCards/page'
 const Projects = () => {
     return (
         <div id='projects' className='p-4 md:px-8 mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  space-x-4 space-y-4  '>
-            <ProjectsCard title="ali" gitlink="any link" prelink="any link" />
-            <ProjectsCard title="ali" gitlink="any link" prelink="any link" />
-            <ProjectsCard title="ali" gitlink="any link" prelink="any link" />
-            <ProjectsCard title="ali" gitlink="any link" prelink="any link" />
+            <ProjectsCard title="Attendance Menegement app" gitlink="any link" prelink="any link" />
+            <ProjectsCard title="AI Mock Interview" gitlink="any link" prelink="any link" />
+            <ProjectsCard title="My portfolio" gitlink="any link" prelink="any link" />
+            <ProjectsCard title="Other project" gitlink="any link" prelink="any link" />
             <ProjectsCard title="ali" gitlink="any link" prelink="any link" />
             <ProjectsCard title="ali" gitlink="any link" prelink="any link" />
         </div>
