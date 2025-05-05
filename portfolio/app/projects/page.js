9c66f4038f5218197d1e3c274130349f40f7a950
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-05-05 23:23:23 +0500

    pass links

--- portfolio/app/projects/page.js
+++ portfolio/app/projects/page.js
@@ -5,12 +5,12 @@ import ProjectsCard from '../(components)/projectCards/page'
 const Projects = () => {
     return (
         <div id='projects' className='p-4 md:px-8 mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  space-x-4 space-y-4  '>
-            <ProjectsCard title="ali" />
-            <ProjectsCard />
-            <ProjectsCard />
-            <ProjectsCard />
-            <ProjectsCard />
-            <ProjectsCard />
+            <ProjectsCard title="ali" gitlink="any link" prelink="any link" />
+            <ProjectsCard title="ali" gitlink="any link" prelink="any link" />
+            <ProjectsCard title="ali" gitlink="any link" prelink="any link" />
+            <ProjectsCard title="ali" gitlink="any link" prelink="any link" />
+            <ProjectsCard title="ali" gitlink="any link" prelink="any link" />
+            <ProjectsCard title="ali" gitlink="any link" prelink="any link" />
         </div>
     )
 }
