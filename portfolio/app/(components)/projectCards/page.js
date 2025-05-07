fd7620f66e7f2a5c53dd35ff14f2b45ee45a3043
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-05-07 22:47:46 +0500

    useing links

--- portfolio/app/(components)/projectCards/page.js
+++ portfolio/app/(components)/projectCards/page.js
@@ -69,3 +69,19 @@ export default function ProjectsCard(proops) {
         </div>
     );
 }
+
+
+{/* <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                    <div className="flex space-x-4">
+                        <button className="bg-white p-3 rounded-full shadow-lg hover:bg-purple-500 hover:text-white transition-colors duration-300">
+                            {proops.gitlink && (<svg className="w-6 h-6" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                            </svg>)}
+                        </button>
+                        <button className="bg-white p-3 rounded-full shadow-lg hover:bg-blue-500 hover:text-white transition-colors duration-300">
+                            {proops.prelink && (<svg className="w-6 h-6" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7"></path>
+                            </svg>)}
+                        </button>
+                    </div>
+                </div> */}
