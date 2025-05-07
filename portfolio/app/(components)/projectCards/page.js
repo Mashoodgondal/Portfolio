502743670c77add69ccd7da501e55039452cf898
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-05-07 22:46:40 +0500

    useing links

--- portfolio/app/(components)/projectCards/page.js
+++ portfolio/app/(components)/projectCards/page.js
@@ -12,20 +12,7 @@ export default function ProjectsCard(proops) {
                     className="w-full h-64 object-cover transition-opacity duration-300 group-hover:opacity-50"
                 />
 
-                {/* <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                    <div className="flex space-x-4">
-                        <button className="bg-white p-3 rounded-full shadow-lg hover:bg-purple-500 hover:text-white transition-colors duration-300">
-                            {proops.gitlink && (<svg className="w-6 h-6" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                            </svg>)}
-                        </button>
-                        <button className="bg-white p-3 rounded-full shadow-lg hover:bg-blue-500 hover:text-white transition-colors duration-300">
-                            {proops.prelink && (<svg className="w-6 h-6" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7"></path>
-                            </svg>)}
-                        </button>
-                    </div>
-                </div> */}
+
                 <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                     <div className="flex space-x-4">
 
--- portfolio/app/projects/page.js
+++ portfolio/app/projects/page.js
@@ -6,7 +6,7 @@ import interview from '../../public/images/interview.jpg'
 const Projects = () => {
     return (
         <div id='projects' className='p-4 md:px-8 mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  space-x-4 space-y-4  '>
-            <ProjectsCard img={attendance} title="Attendance Menegement app" gitlink="https://github.com/Mashoodgondal/Students-attendance.git" prelink="students-attendance-hnyw.vercel.app" />
+            <ProjectsCard img={attendance} title="Attendance Menegement app" gitlink="https://github.com/Mashoodgondal/Students-attendance.git" prelink="https://students-attendance-hnyw.vercel.app" />
             <ProjectsCard img={interview} title="AI Mock Interview" gitlink="any link" prelink="any link" />
             <ProjectsCard title="My portfolio" gitlink="any link" prelink="any link" />
             <ProjectsCard title="Other project" gitlink="any link" prelink="any link" />
